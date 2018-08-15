d9c25b37ef0b84b1fbe985856640a55d6fc9e2d4
Tobias Bräutigam <tbraeutigam@gmail.com>
2018-08-15 21:52:23 +0200

    fix basic setup

--- apply.sh
+++ apply.sh
@@ -2,15 +2,17 @@
 # if [[ $(oc whoami) != "developer" ]]; then
 #     oc login -u developer
 # fi
-if [[ $(oc project -q) != "deernation" ]]; then
-    oc project deernation || return 1
+if [[ $(oc project -q) != "default" ]]; then
+    oc project default || return 1
 fi
 #oc config use-context `oc config get-contexts --no-headers=true --output=name | grep deernation | grep admin` || return 1
 
 for file in kubernetes/*.yaml; do
     [ -e "$file" ] || continue
-    istioctl kube-inject -f "$file" | oc apply -n deernation -f -
+    istioctl kube-inject -f "$file" | oc apply -n default -f -
 done
+
+sleep 5
 
 printf "Waiting for DeerNation system to get ready."
 while [[ $(oc get pods --no-headers | grep -v Running | grep -v Completed | wc -l) != "0" ]]; do
--- start-minishift.sh
+++ start-minishift.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/env bash
 
+# check i istioctl is available
+if [[ $(command -v istioctl) == "" ]]; then
+    echo "istioctl command is not available in your system. Please download the latest release of istio an make istioctl available in your PATH."
+    return 1
+fi
+
+if [[ $(command -v helm) == "" ]]; then
+    echo "helm command is not available in your system. Please download the latest release of helm an make it available in your PATH."
+    return 1
+fi
+
 if [ -d ~/.minishift ]; then
     read -p "Installation is active. Remove it? [y/n]" -n 1 -s res
     echo
@@ -34,17 +45,44 @@ echo "applying bash-completion..."
 source ./.minishift-completion
 source ./.oc-completion
 
+pushd .
+cd istio/
+TAG=1.0.0
+HUB=gcr.io/istio-release
+install/updateVersion.sh -a ${HUB},${TAG} >/dev/null 2>&1
+cat install/kubernetes/namespace.yaml > install/kubernetes/istio-demo.yaml
+helm template --set global.tag=${TAG} \
+		--namespace=istio-system \
+		--set global.hub=${HUB} \
+		--set prometheus.enabled=true \
+        --set sidecarInjectorWebhook.enabled=true \
+        --set sidecarInjectorWebhook.enableNamespacesByDefault=false \
+        --set grafana.enabled=true \
+        --set tracing.enabled=true \
+        --set servicegraph.enabled=true \
+        --set galley.enabled=true \
+		install/kubernetes/helm/istio >> install/kubernetes/istio-demo.yaml
+popd
+
+if [[ ! -f istio/install/kubernetes/istio-demo.yaml ]]; then
+    echo "istio/install/kubernetes/istio-demo.yaml file not found"
+    return 1
+elif [[ $(wc -l istio/install/kubernetes/istio-demo.yaml | awk '{print $1}') -le 100 ]]; then
+    echo "istio/install/kubernetes/istio-demo.yaml file is too small, something must have gone wrong when generating it."
+    return 1
+fi
+
 oc login -u admin -p admin
+
 # install istio
 pushd .
 cd istio/install/kubernetes/ansible/
 ansible-playbook main.yml || return 1
 popd
-oc adm policy add-scc-to-user anyuid -z default -n deernation
-oc adm policy add-scc-to-user privileged -z default -n deernation
-oc adm policy add-scc-to-user privileged developer
-oc adm policy add-role-to-user admin developer -n istio-system
+oc adm policy add-scc-to-user anyuid -z default -n default
+oc adm policy add-scc-to-user privileged -z default -n default
 oc adm policy add-cluster-role-to-user cluster-admin -z default
+
 
 printf "Waiting for istio system to get ready."
 while [[ $(oc get pods -n istio-system --no-headers | grep -v Running | grep -v Completed | wc -l) != "0" ]]; do
@@ -63,13 +101,20 @@ GRAFANA=$(oc get route grafana -n istio-system -o jsonpath='{.spec.host}{"\n"}')
 ZIPKIN=$(oc get route zipkin -n istio-system -o jsonpath='{.spec.host}{"\n"}')
 
 # oc login -u developer
-if [[ $(oc get projects -o=name | grep deernation) == "" ]]; then
-    oc new-project deernation --display-name=DeerNation
-fi;
+# if [[ $(oc get projects -o=name | grep deernation) == "" ]]; then
+#     oc new-project deernation --display-name=DeerNation
+# fi;
 
 source ./apply.sh
 
-oc port-forward -n istio-system `oc get pods -n istio-system --selector=app=istio-ingressgateway -o name | cut -f2 -d /` 8080:80 &
+# oc port-forward -n istio-system `oc get pods -n istio-system --selector=app=istio-ingressgateway -o name | cut -f2 -d /` 8080:80 &
+# export INGRESS_HOST=$(oc -n istio-system get service istio-ingressgateway -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# export INGRESS_PORT=$(oc -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="http2")].port}')
+# export SECURE_INGRESS_PORT=$(oc -n istio-system get service istio-ingressgateway -o jsonpath='{.spec.ports[?(@.name=="https")].port}')
+
+oc expose svc istio-ingressgateway -n istio-system
+export GATEWAY_URL=$(oc get route istio-ingressgateway -n istio-system -o jsonpath='{.spec.host}{"\n"}')
+echo "Link to GUI: http://${GATEWAY_URL}"
 
 # enable port-forwarding for dgraph (server + ratel)
 #echo "enabling port-forwarding..."
